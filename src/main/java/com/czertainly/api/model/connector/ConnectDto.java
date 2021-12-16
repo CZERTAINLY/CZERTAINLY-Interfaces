0bf7c2a363071e37713d678433d7053d6392e267
3KeyPradeep <pradeep.saminathan@3key.company>
2021-12-16 08:42:08 +0100

    Remove duplicate end point from connect DTO
    Removed the duplicate endpoints from the connect DTO. The end points will be provided by the Function Group.

--- src/main/java/com/czertainly/api/model/connector/ConnectDto.java
+++ src/main/java/com/czertainly/api/model/connector/ConnectDto.java
@@ -1,11 +1,8 @@
 package com.czertainly.api.model.connector;
 
-import java.util.List;
-
 public class ConnectDto {
 
     private FunctionGroupDto functionGroup;
-    private List<EndpointDto> endpoints;
 
     public FunctionGroupDto getFunctionGroup() {
         return functionGroup;
@@ -13,13 +10,5 @@ public class ConnectDto {
 
     public void setFunctionGroup(FunctionGroupDto functionGroup) {
         this.functionGroup = functionGroup;
-    }
-
-    public List<EndpointDto> getEndpoints() {
-        return endpoints;
-    }
-
-    public void setEndpoints(List<EndpointDto> endpoints) {
-        this.endpoints = endpoints;
     }
 }
