348526547bf596fc1ffe7a89014d96d9a60d16bc
3KeyPradeep <pradeep.saminathan@3key.company>
2021-12-17 17:43:28 +0100

    Authority Refactor
    Authority Refactor

--- src/main/java/com/czertainly/api/model/ca/CAInstanceDto.java
+++ src/main/java/com/czertainly/api/model/ca/CAInstanceDto.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 public class CAInstanceDto extends NameAndUuidDto {
 
-
     @Schema(description = "List of Authority Attributes",
             required = true)
     private List<AttributeDefinition> attributes;
