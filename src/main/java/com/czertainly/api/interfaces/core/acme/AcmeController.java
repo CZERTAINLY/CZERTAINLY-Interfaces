87fe31fd584123c439a240cc0481d12342a1401f
3KeyPradeep <pradeep.saminathan@3key.company>
2022-02-02 22:00:34 +0530

    Update request methods for controllers
    Update the request methods for the ACME related controllers to generate documentation

--- src/main/java/com/czertainly/api/interfaces/core/acme/AcmeController.java
+++ src/main/java/com/czertainly/api/interfaces/core/acme/AcmeController.java
@@ -63,7 +63,7 @@ public interface AcmeController {
 
     @Operation(summary = "New Nonce")
     @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "New Nonce retrieved")})
-    @RequestMapping(path = "/new-nonce")
+    @RequestMapping(path = "/new-nonce", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     ResponseEntity<?> getNonce(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName);
 
@@ -100,7 +100,7 @@ public interface AcmeController {
 
     @Operation(summary = "List Orders")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Orders list retrieved")})
-    @RequestMapping("/orders/{accountId}")
+    @RequestMapping(path = "/orders/{accountId}", method = RequestMethod.POST)
     ResponseEntity<List<Order>> listOrders(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName, @Parameter(description = "Account Id") @PathVariable String accountId) throws NotFoundException, AcmeProblemDocumentException;
 
     @Operation(summary = "Get Authorizations for an Order")
