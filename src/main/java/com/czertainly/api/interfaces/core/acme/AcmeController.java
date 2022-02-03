2d5b4c583bcfad110b47f6f1f48a18bcff93191d
3KeyPradeep <pradeep.saminathan@3key.company>
2022-02-03 14:55:04 +0530

    Update external documentation
    Update external documentation

--- src/main/java/com/czertainly/api/interfaces/core/acme/AcmeController.java
+++ src/main/java/com/czertainly/api/interfaces/core/acme/AcmeController.java
@@ -6,6 +6,7 @@ import com.czertainly.api.exception.ConnectorException;
 import com.czertainly.api.exception.NotFoundException;
 import com.czertainly.api.model.core.acme.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -61,85 +62,85 @@ import java.util.Map;
         })
 public interface AcmeController {
 
-    @Operation(summary = "Get Directory information")
+    @Operation(summary = "Get Directory information", externalDocs = @ExternalDocumentation(description = "RFC 8555, section 7.1.1", url = "https://datatracker.ietf.org/doc/html/rfc8555#section-7.1.1"))
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Directory information retrieved")})
     @RequestMapping(path = "/directory", method = RequestMethod.GET, produces = {"application/json"})
     ResponseEntity<Directory> getDirectory(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName) throws NotFoundException, AcmeProblemDocumentException;
 
-    @Operation(summary = "New Nonce")
+    @Operation(summary = "New Nonce", externalDocs = @ExternalDocumentation(description = "RFC 8555, section 7.2", url = "https://datatracker.ietf.org/doc/html/rfc8555#section-7.2"))
     @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "New Nonce retrieved", content = @Content(schema = @Schema(hidden = true)))})
     @RequestMapping(path = "/new-nonce", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     ResponseEntity<?> getNonce(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName);
 
-    @Operation(summary = "New Nonce")
+    @Operation(summary = "New Nonce", externalDocs = @ExternalDocumentation(description = "RFC 8555, section 7.2", url = "https://datatracker.ietf.org/doc/html/rfc8555#section-7.2"))
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "New Nonce retrieved", content = @Content(schema = @Schema(hidden = true)))})
     @RequestMapping(path = "/new-nonce", method = RequestMethod.HEAD)
     ResponseEntity<?> headNonce(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName);
 
-    @Operation(summary = "Create Account")
+    @Operation(summary = "Create Account", externalDocs = @ExternalDocumentation(description = "RFC 8555, section 7.3", url = "https://datatracker.ietf.org/doc/html/rfc8555#section-7.3"))
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Existing Account retrieved"),
             @ApiResponse(responseCode = "201", description = "New Account created")})
     @RequestMapping(path = "/new-account", method = RequestMethod.POST, consumes = {"application/jose+json"}, produces = {"application/json"})
     ResponseEntity<Account> newAccount(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName, @io.swagger.v3.oas.annotations.parameters.RequestBody(
             description = "New Account JWS payload", content = @Content(schema = @Schema(implementation = NewAccountRequest.class))) @RequestBody String jwsBody) throws AcmeProblemDocumentException, NotFoundException;
 
-    @Operation(summary = "Update Account")
+    @Operation(summary = "Update Account", externalDocs = @ExternalDocumentation(description = "RFC 8555, section 7.3.2", url = "https://datatracker.ietf.org/doc/html/rfc8555#section-7.3.2"))
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "ACME Account updated")})
     @RequestMapping(path = "/acct/{accountId}", method = RequestMethod.POST, consumes = {"application/jose+json"}, produces = {"application/json"})
     ResponseEntity<Account> updateAccount(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName, @Parameter(description = "Account ID") @PathVariable String accountId, @io.swagger.v3.oas.annotations.parameters.RequestBody(
             description = "Account JWS payload", content = @Content(schema = @Schema(implementation = NewAccountRequest.class))) @RequestBody String jwsBody) throws AcmeProblemDocumentException, NotFoundException;
 
-    @Operation(summary = "Key Rollover")
+    @Operation(summary = "Key Rollover", externalDocs = @ExternalDocumentation(description = "RFC 8555, section 7.3.5", url = "https://datatracker.ietf.org/doc/html/rfc8555#section-7.3.5"))
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Account key updated"),
             @ApiResponse(responseCode = "409", description = "Conflict. Key already exists", content = @Content(mediaType = "application/problem+json", schema = @Schema(implementation = ProblemDocument.class)))})
     @RequestMapping(path = "/key-change", method = RequestMethod.POST, consumes = {"application/jose+json"}, produces = {"application/json"})
     ResponseEntity<?> keyRollover(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName, @io.swagger.v3.oas.annotations.parameters.RequestBody(
             description = "Key Rollover JWS payload", content = @Content(schema = @Schema(implementation = KeyRollover.class))) @RequestBody String jwsBody) throws NotFoundException, AcmeProblemDocumentException;
 
-    @Operation(summary = "Request new Order")
+    @Operation(summary = "Request new Order", externalDocs = @ExternalDocumentation(description = "RFC 8555, section 7.4", url = "https://datatracker.ietf.org/doc/html/rfc8555#section-7.4"))
     @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "New Order request created")})
     @RequestMapping(path = "/new-order", method = RequestMethod.POST, consumes = {"application/jose+json"}, produces = {"application/json"})
     ResponseEntity<Order> newOrder(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName, @io.swagger.v3.oas.annotations.parameters.RequestBody(
             description = "New Order JWS payload", content = @Content(schema = @Schema(implementation = CertificateIssuanceRequest.class))) @RequestBody String jwsBody) throws AcmeProblemDocumentException, NotFoundException;
 
-    @Operation(summary = "List Orders")
+    @Operation(summary = "List Orders", externalDocs = @ExternalDocumentation(description = "RFC 8555, section 7.1.2.1", url = "https://datatracker.ietf.org/doc/html/rfc8555#section-7.1.2.1"))
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Orders list retrieved")})
     @RequestMapping(path = "/orders/{accountId}", method = RequestMethod.POST, consumes = {"application/jose+json"}, produces = {"application/json"})
     @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "List Orders JWS Payload", required = true, content = @Content(mediaType = "application/jose+json", examples = {@ExampleObject(value = "")}))
     ResponseEntity<List<Order>> listOrders(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName, @Parameter(description = "Account Id") @PathVariable String accountId) throws NotFoundException, AcmeProblemDocumentException;
 
-    @Operation(summary = "Get Authorizations for an Order")
+    @Operation(summary = "Get Authorizations for an Order", externalDocs = @ExternalDocumentation(description = "RFC 8555, section 7.5", url = "https://datatracker.ietf.org/doc/html/rfc8555#section-7.5"))
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Authorizations retrieved")})
     @RequestMapping(path = "/authz/{authorizationId}", method = RequestMethod.POST, consumes = {"application/jose+json"}, produces = {"application/json"})
     @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Get Authorization of Order JWS Payload", required = true, content = @Content(mediaType = "application/jose+json", examples = {@ExampleObject(value = "")}))
     ResponseEntity<Authorization> getAuthorizations(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName, @Parameter(description = "Authorization Id") @PathVariable String authorizationId, @RequestBody String jwsBody) throws NotFoundException, AcmeProblemDocumentException;
 
-    @Operation(summary = "Validate Challenge")
+    @Operation(summary = "Validate Challenge", externalDocs = @ExternalDocumentation(description = "RFC 8555, section 7.5.1", url = "https://datatracker.ietf.org/doc/html/rfc8555#section-7.5.1"))
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Challenge validation initiated")})
     @RequestMapping(path = "/chall/{challengeId}", method = RequestMethod.POST, consumes = {"application/jose+json"}, produces = {"application/json"})
     @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Initiate Challenge validation JWS Payload", required = true, content = @Content(schema = @Schema(implementation = Map.class), mediaType = "application/jose+json", examples = {@ExampleObject(value = "{}")}))
     ResponseEntity<Challenge> validateChallenge(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName, @Parameter(description = "Challenge Id") @PathVariable String challengeId) throws NotFoundException, NoSuchAlgorithmException, InvalidKeySpecException, AcmeProblemDocumentException;
 
-    @Operation(summary = "Get Order details")
+    @Operation(summary = "Get Order details", externalDocs = @ExternalDocumentation(description = "RFC 8555, section 7.1.3", url = "https://datatracker.ietf.org/doc/html/rfc8555#section-7.1.3"))
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Order details retrieved")})
     @RequestMapping(path = "/order/{orderId}", method = RequestMethod.POST, consumes = {"application/jose+json"}, produces = {"application/json"})
     @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Get Order details payload", required = true, content = @Content(mediaType = "application/jose+json", examples = {@ExampleObject(value = "")}))
     ResponseEntity<Order> getOrder(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName, @Parameter(description = "Order Id") @PathVariable String orderId) throws NotFoundException, AcmeProblemDocumentException;
 
-    @Operation(summary = "Finalize Order")
+    @Operation(summary = "Finalize Order", externalDocs = @ExternalDocumentation(description = "RFC 8555, section 7.4", url = "https://datatracker.ietf.org/doc/html/rfc8555#:~:text=the%20order%20resource%27s-,finalize,-URL.%0A%20%20%20The%20POST"))
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Order finalized")})
     @RequestMapping(path = "/order/{orderId}/finalize", method = RequestMethod.POST, consumes = {"application/jose+json"}, produces = {"application/json"})
     ResponseEntity<Order> finalize(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName, @Parameter(description = "Order Id") @PathVariable String orderId, @io.swagger.v3.oas.annotations.parameters.RequestBody(
             description = "Finalize Order JWS payload", content = @Content(schema = @Schema(implementation = CertificateFinalizeRequest.class))) @RequestBody String jwsBody) throws AcmeProblemDocumentException, ConnectorException, JsonProcessingException, CertificateException, AlreadyExistException;
 
-    @Operation(summary = "Download Certificate")
+    @Operation(summary = "Download Certificate", externalDocs = @ExternalDocumentation(description = "RFC 8555, section 7.4.2", url = "https://datatracker.ietf.org/doc/html/rfc8555#section-7.4.2"))
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Certificate content retrieved",content = @Content(schema = @Schema(hidden = true)))})
     @RequestMapping(path = "/cert/{certificateId}", method = RequestMethod.POST, consumes = {"application/jose+json"}, produces = {"application/pem-certificate-chain"})
     @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Download Certificate Payload", required = true, content = @Content(mediaType = "application/jose+json", examples = {@ExampleObject(value = "")}))
     ResponseEntity<Resource> downloadCertificate(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName, @Parameter(description = "Certificate Id") @PathVariable String certificateId) throws NotFoundException, CertificateException;
 
-    @Operation(summary = "Revoke Certificate")
+    @Operation(summary = "Revoke Certificate", externalDocs = @ExternalDocumentation(description = "RFC 8555, section 7.6", url = "https://datatracker.ietf.org/doc/html/rfc8555#section-7.6"))
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Certificate revoked", content = @Content(schema = @Schema(hidden = true)))})
     @RequestMapping(path = "/revoke-cert", method = RequestMethod.POST, consumes = {"application/jose+json"}, produces = {"application/json"})
     ResponseEntity<?> revokeCertificate(@Parameter(description = "ACME Profile name") @PathVariable String acmeProfileName, @io.swagger.v3.oas.annotations.parameters.RequestBody(
