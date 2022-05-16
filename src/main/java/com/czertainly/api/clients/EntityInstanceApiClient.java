ca7540583b59017c7810741f845974661256306b
3keyroman <roman.cinkais@3key.company>
2022-05-16 12:16:16 +0200

    Added Entity instance API client

---
+++ src/main/java/com/czertainly/api/clients/EntityInstanceApiClient.java
@@ -0,0 +1,118 @@
+package com.czertainly.api.clients;
+
+import com.czertainly.api.exception.ConnectorException;
+import com.czertainly.api.exception.ValidationException;
+import com.czertainly.api.model.common.AttributeDefinition;
+import com.czertainly.api.model.common.RequestAttributeDto;
+import com.czertainly.api.model.connector.entity.EntityInstanceDto;
+import com.czertainly.api.model.connector.entity.EntityInstanceRequestDto;
+import com.czertainly.api.model.core.connector.ConnectorDto;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class EntityInstanceApiClient extends BaseApiClient {
+
+    private static final String ENTITY_INSTANCE_BASE_CONTEXT = "/v1/entityProvider/entities";
+    private static final String ENTITY_INSTANCE_IDENTIFIED_CONTEXT = ENTITY_INSTANCE_BASE_CONTEXT + "/{entityUuid}";
+    private static final String ENTITY_INSTANCE_LOCATION_ATTRS_CONTEXT = ENTITY_INSTANCE_IDENTIFIED_CONTEXT + "/location/attributes";
+    private static final String ENTITY_INSTANCE_LOCATION_ATTRS_VALIDATE_CONTEXT = ENTITY_INSTANCE_LOCATION_ATTRS_CONTEXT + "/validate";
+
+    private static final ParameterizedTypeReference<List<RequestAttributeDto>> ATTRIBUTE_LIST_TYPE_REF = new ParameterizedTypeReference<>() {
+    };
+
+    public EntityInstanceApiClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public List<EntityInstanceDto> listEntityInstances(ConnectorDto connector) throws ConnectorException {
+        WebClient.RequestBodyUriSpec request = prepareRequest(HttpMethod.GET, connector.getAuthType(), connector.getAuthAttributes());
+
+        return processRequest(r -> r
+                .uri(connector.getUrl() + ENTITY_INSTANCE_BASE_CONTEXT)
+                .retrieve()
+                .toEntityList(EntityInstanceDto.class)
+                .block().getBody(),
+                request,
+                connector);
+    }
+
+    public EntityInstanceDto getEntityInstance(ConnectorDto connector, String entityUuid) throws ConnectorException {
+        WebClient.RequestBodyUriSpec request = prepareRequest(HttpMethod.GET, connector.getAuthType(), connector.getAuthAttributes());
+
+        return processRequest(r -> r
+                .uri(connector.getUrl() + ENTITY_INSTANCE_IDENTIFIED_CONTEXT, entityUuid)
+                .retrieve()
+                .toEntity(EntityInstanceDto.class)
+                .block().getBody(),
+                request,
+                connector);
+    }
+
+    public EntityInstanceDto createEntityInstance(ConnectorDto connector, EntityInstanceRequestDto requestDto) throws ConnectorException {
+        WebClient.RequestBodyUriSpec request = prepareRequest(HttpMethod.POST, connector.getAuthType(), connector.getAuthAttributes());
+
+        return processRequest(r -> r
+                .uri(connector.getUrl() + ENTITY_INSTANCE_BASE_CONTEXT)
+                .body(Mono.just(requestDto), EntityInstanceRequestDto.class)
+                .retrieve()
+                .toEntity(EntityInstanceDto.class)
+                .block().getBody(),
+                request,
+                connector);
+    }
+
+    public EntityInstanceDto updateEntityInstance(ConnectorDto connector, String entityUuid, EntityInstanceRequestDto requestDto) throws ConnectorException {
+        WebClient.RequestBodyUriSpec request = prepareRequest(HttpMethod.POST, connector.getAuthType(), connector.getAuthAttributes());
+
+        return processRequest(r -> r
+                .uri(connector.getUrl() + ENTITY_INSTANCE_IDENTIFIED_CONTEXT, entityUuid)
+                .body(Mono.just(requestDto), EntityInstanceRequestDto.class)
+                .retrieve()
+                .toEntity(EntityInstanceDto.class)
+                .block().getBody(),
+                request,
+                connector);
+    }
+
+    public void removeEntityInstance(ConnectorDto connector, String entityUuid) throws ConnectorException {
+        WebClient.RequestBodyUriSpec request = prepareRequest(HttpMethod.DELETE, connector.getAuthType(), connector.getAuthAttributes());
+
+        processRequest(r -> r
+                .uri(connector.getUrl() + ENTITY_INSTANCE_IDENTIFIED_CONTEXT, entityUuid)
+                .retrieve()
+                .toEntity(Void.class)
+                .block().getBody(),
+                request,
+                connector);
+    }
+
+
+    public List<AttributeDefinition> listLocationAttributes(ConnectorDto connector, String entityUuid) throws ConnectorException {
+        WebClient.RequestBodyUriSpec request = prepareRequest(HttpMethod.GET, connector.getAuthType(), connector.getAuthAttributes());
+
+        return processRequest(r -> r
+                .uri(connector.getUrl() + ENTITY_INSTANCE_LOCATION_ATTRS_CONTEXT, entityUuid)
+                .retrieve()
+                .toEntityList(AttributeDefinition.class)
+                .block().getBody(),
+                request,
+                connector);
+    }
+
+    public Boolean validateLocationAttributes(ConnectorDto connector, String entityUuid, List<RequestAttributeDto> attributes) throws ValidationException, ConnectorException {
+        WebClient.RequestBodyUriSpec request = prepareRequest(HttpMethod.POST, connector.getAuthType(), connector.getAuthAttributes());
+
+        return processRequest(r -> r
+                .uri(connector.getUrl() + ENTITY_INSTANCE_LOCATION_ATTRS_VALIDATE_CONTEXT, entityUuid)
+                .body(Mono.just(attributes), ATTRIBUTE_LIST_TYPE_REF)
+                .retrieve()
+                .toEntity(Boolean.class)
+                .block().getBody(),
+                request,
+                connector);
+    }
+}
