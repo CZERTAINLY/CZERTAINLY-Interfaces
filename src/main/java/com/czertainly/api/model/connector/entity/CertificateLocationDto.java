edc6c7d15e9a99b087c6f696be73a58f765d6b60
3keyroman <roman.cinkais@3key.company>
2022-06-03 13:58:13 +0200

    Update model and dto for push and csr attributes

--- src/main/java/com/czertainly/api/model/connector/entity/CertificateLocationDto.java
+++ src/main/java/com/czertainly/api/model/connector/entity/CertificateLocationDto.java
@@ -1,10 +1,12 @@
 package com.czertainly.api.model.connector.entity;
 
+import com.czertainly.api.model.common.RequestAttributeDto;
 import com.czertainly.api.model.core.certificate.CertificateType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+import java.util.List;
 import java.util.Map;
 
 public class CertificateLocationDto {
@@ -26,6 +28,16 @@ public class CertificateLocationDto {
     @Schema(description = "If the Certificate in Location has associated private key",
             required = true)
     private boolean withKey;
+
+    @Schema(
+            description = "List of Attributes to replace Certificate"
+    )
+    private List<RequestAttributeDto> pushAttributes;
+
+    @Schema(
+            description = "List of Attributes to renew Certificate"
+    )
+    private List<RequestAttributeDto> csrAttributes;
 
     public String getCertificateData() {
         return certificateData;
@@ -59,12 +71,30 @@ public class CertificateLocationDto {
         this.withKey = withKey;
     }
 
+    public List<RequestAttributeDto> getPushAttributes() {
+        return pushAttributes;
+    }
+
+    public void setPushAttributes(List<RequestAttributeDto> pushAttributes) {
+        this.pushAttributes = pushAttributes;
+    }
+
+    public List<RequestAttributeDto> getCsrAttributes() {
+        return csrAttributes;
+    }
+
+    public void setCsrAttributes(List<RequestAttributeDto> csrAttributes) {
+        this.csrAttributes = csrAttributes;
+    }
+
     @Override
     public String toString() {
         return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
                 .append("certificateData", certificateData)
                 .append("metadata", metadata)
                 .append("certificateType", certificateType)
+                .append("pushAttributes", pushAttributes)
+                .append("csrAttributes", csrAttributes)
                 .append("withKey", withKey)
                 .toString();
     }
