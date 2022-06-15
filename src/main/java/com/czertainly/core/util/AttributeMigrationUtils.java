1ce7c3b91789fa504fbb66022ed8ea8a0e705a7f
3KeyPradeep <pradeep.saminathan@3key.company>
2022-06-15 13:53:16 +0100

    Update AttributeMigrationUtils.java

--- src/main/java/com/czertainly/core/util/AttributeMigrationUtils.java
+++ src/main/java/com/czertainly/core/util/AttributeMigrationUtils.java
@@ -9,6 +9,8 @@ import com.czertainly.api.model.common.attribute.content.JsonAttributeContent;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,8 +19,10 @@ import java.util.List;
 import java.util.Map;
 
 public class AttributeMigrationUtils {
+    private static Logger logger = LoggerFactory.getLogger(AttributeMigrationUtils.class);
 
     public static List<String> getMigrationCommands(ResultSet rows, String tableName, String columnName) throws SQLException, JsonProcessingException {
+        logger.info("Migrating Table: {}, Column: {}", tableName, columnName);
         ObjectMapper mapper = new ObjectMapper();
         List<String> migrationCommands = new ArrayList<>();
         while (rows.next()) {
@@ -26,6 +30,7 @@ public class AttributeMigrationUtils {
             List<Map<String, Object>> oldAttributeValue = mapper.readValue(rows.getString(columnName), new TypeReference<>() {
             });
             for (Map<String, Object> item : oldAttributeValue) {
+                logger.debug("Migrating Attribute: {}", item);
                 attributeDefinitions.add(getNewAttributes(item));
             }
             String updateCommand;
@@ -35,6 +40,7 @@ public class AttributeMigrationUtils {
             }else {
                 updateCommand = "UPDATE " + tableName + " SET " + columnName + " = '" + serializedAttributes + "' WHERE id = " + rows.getString("id") + ";";
             }
+            logger.debug("Update Command: {}", updateCommand);
             migrationCommands.add(updateCommand);
         }
         return migrationCommands;
@@ -42,8 +48,8 @@ public class AttributeMigrationUtils {
 
     public static AttributeDefinition getNewAttributes(Map<String, Object> oldAttribute) {
         AttributeDefinition attributeDefinition = new AttributeDefinition();
-        attributeDefinition.setLabel((String) oldAttribute.get("label"));
-        attributeDefinition.setName((String) oldAttribute.get("name"));
+        attributeDefinition.setLabel(((String) oldAttribute.get("label")).replaceAll("'", "''"));
+        attributeDefinition.setName(((String) oldAttribute.get("name")).replaceAll("'", "''"));
         attributeDefinition.setUuid((String) oldAttribute.get("uuid"));
         attributeDefinition.setReadOnly((Boolean) oldAttribute.get("readOnly"));
         if (oldAttribute.get("required") != null) {
@@ -53,8 +59,6 @@ public class AttributeMigrationUtils {
         }
         if (oldAttribute.get("description") != null) {
             attributeDefinition.setDescription(((String) oldAttribute.get("description")).replaceAll("'", "''"));
-        } else {
-            attributeDefinition.setDescription(null);
         }
         if (oldAttribute.get("visible") != null) {
             attributeDefinition.setVisible((Boolean) oldAttribute.get("visible"));
@@ -67,31 +71,32 @@ public class AttributeMigrationUtils {
             attributeDefinition.setMultiSelect(false);
         }
         if (oldAttribute.get("validationRegex") != null) {
-            attributeDefinition.setValidationRegex((String) oldAttribute.get("validationRegex"));
+            attributeDefinition.setValidationRegex(((String) oldAttribute.get("validationRegex")).replaceAll("'", "''"));
         } else {
             attributeDefinition.setMultiSelect(false);
         }
         if (oldAttribute.get("value") != null) {
             attributeDefinition.setContent(getAttributeValue(oldAttribute.get("value"), (String) oldAttribute.get("type")));
-        } else {
-            attributeDefinition.setContent(null);
         }
         attributeDefinition.setType(getAttributeType(oldAttribute.get("value"), (String) oldAttribute.get("type")));
         attributeDefinition.setList(isAttributeList((String) oldAttribute.get("type")));
         if (oldAttribute.get("attributeCallback") != null) {
             ObjectMapper mapper = new ObjectMapper();
+            String callbackAsString = null;
             try {
-                String callbackAsString = mapper.writeValueAsString(oldAttribute.get("attributeCallback"));
+                callbackAsString = mapper.writeValueAsString(oldAttribute.get("attributeCallback"));
                 AttributeCallback attributeCallback = mapper.readValue(callbackAsString, AttributeCallback.class);
                 attributeDefinition.setAttributeCallback(attributeCallback);
             } catch (JsonProcessingException e) {
-                e.printStackTrace();
+                logger.error("Unable to serialize callback, {}", oldAttribute.get("attributeCallback"));
             }
         }
+        logger.debug("New definition: {}", attributeDefinition);
         return attributeDefinition;
     }
 
     private static AttributeType getAttributeType(Object oldValue, String oldAttributeType) {
+        logger.debug("Old Value: {} Old Type:", oldValue, oldAttributeType);
         if (oldAttributeType.equals("LIST")) {
             if (oldValue instanceof List) {
                 if (((List<?>) oldValue).get(0) instanceof String) {
@@ -124,6 +129,7 @@ public class AttributeMigrationUtils {
     }
 
     private static Object getAttributeValue(Object oldValue, String oldType) {
+        logger.debug("Old Value: {} Old Type:", oldValue, oldType);
         if (oldType.equals("FILE")) {
             return new FileAttributeContent() {{
                 setValue((String) oldValue);
@@ -145,12 +151,30 @@ public class AttributeMigrationUtils {
             return new BaseAttributeContent<Integer>() {{
                 setValue((Integer) oldValue);
             }};
-        } else if (oldValue instanceof List || oldType.equals("LIST")) {
+        } else if (oldValue instanceof List || oldType.equals("LIST") || oldType.equals("CREDENTIAL")) {
             if (oldValue instanceof List) {
                 if (((List<?>) oldValue).get(0) instanceof String) {
                     return new BaseAttributeContent<>() {{
                         setValue(oldValue);
                     }};
+                }else if(((List<?>) oldValue).get(0) instanceof Map) {
+                    for (Map<String, Object> insValue : (List<Map<String, Object>>)oldValue) {
+                        String primKey;
+                        if (insValue.containsKey("name")) {
+                            primKey = (String) insValue.get("name");
+                        } else {
+                            try {
+                                primKey = (String) insValue.get(insValue.keySet().stream().findFirst().orElse("error"));
+                            } catch (Exception e) {
+                                primKey = "error";
+                            }
+                        }
+                        String finalPrimKey = primKey;
+                        return new JsonAttributeContent() {{
+                            setValue(finalPrimKey);
+                            setData(oldValue);
+                        }};
+                    }
                 }
             } else {
                 String primKey;
@@ -171,6 +195,6 @@ public class AttributeMigrationUtils {
                 }};
             }
         }
-        return null;
+        throw new RuntimeException("Unable to recognize value");
     }
 }
