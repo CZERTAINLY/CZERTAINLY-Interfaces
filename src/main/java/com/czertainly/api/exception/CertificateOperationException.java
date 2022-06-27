7053aebb43ff8d7395d5ea4f8d988075dffc8be7
Pradeep Saminathan <76426163+3KeyPradeep@users.noreply.github.com>
2022-06-27 13:06:49 +0200

    Add certificate operation exception
    Updated the logic for the following items.
    1. Fix attribute value when the attributes are null
    2. Update Exceptions for client operations API
    3. Add Certificate Operation Exception to handle any exception raised during the certificate operation

---
+++ src/main/java/com/czertainly/api/exception/CertificateOperationException.java
@@ -0,0 +1,13 @@
+package com.czertainly.api.exception;
+
+public class CertificateOperationException extends Exception {
+
+    public CertificateOperationException() {
+        super();
+    }
+
+    public CertificateOperationException(String message) {
+        super(message);
+    }
+
+}
