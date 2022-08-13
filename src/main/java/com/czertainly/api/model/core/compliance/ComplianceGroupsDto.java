7417fe46a94270394e8757f38663678197325a48
3KeyPradeep <pradeep.saminathan@3key.company>
2022-08-13 14:45:21 +0100

    Add description to Compliance Group when displaying details
    Add description to Compliance Group when displaying details

---
+++ src/main/java/com/czertainly/api/model/core/compliance/ComplianceGroupsDto.java
@@ -0,0 +1,28 @@
+package com.czertainly.api.model.core.compliance;
+
+import com.czertainly.api.model.common.NameAndUuidDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class ComplianceGroupsDto extends NameAndUuidDto {
+    @Schema(description = "Description of the group", example = "Sample group description")
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("uuid", uuid)
+                .append("name", name)
+                .append("description", description)
+                .toString();
+    }
+}
