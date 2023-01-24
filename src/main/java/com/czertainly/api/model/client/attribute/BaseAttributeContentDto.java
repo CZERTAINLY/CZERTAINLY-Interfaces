fd51487e572fb1050afc092cf12eed447bf7d821
Ľubomír Wassermann <76479559+lubomirw@users.noreply.github.com>
2023-01-24 09:49:18 +0100

    Define default BaseAttributeContent schema DTO

---
+++ src/main/java/com/czertainly/api/model/client/attribute/BaseAttributeContentDto.java
@@ -0,0 +1,50 @@
+package com.czertainly.api.model.client.attribute;
+
+import com.czertainly.api.model.common.attribute.v2.content.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(
+        description = "Base Attribute content definition",
+        type = "object",
+        oneOf = {
+                BooleanAttributeContent.class,
+                CredentialAttributeContent.class,
+                DateAttributeContent.class,
+                DateTimeAttributeContent.class,
+                FileAttributeContent.class,
+                FloatAttributeContent.class,
+                IntegerAttributeContent.class,
+                ObjectAttributeContent.class,
+                SecretAttributeContent.class,
+                StringAttributeContent.class,
+                TextAttributeContent.class,
+                TimeAttributeContent.class
+        }
+)
+public class BaseAttributeContentDto {
+    @Schema(description = "Content Reference")
+    private String reference;
+
+    @Schema(description = "Content Data", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Object data;
+
+    public BaseAttributeContentDto() {
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+}
