70490bd534017a82000553cda30e5bed3b2d9478
Pradeep Saminathan <76426163+3KeyPradeep@users.noreply.github.com>
2023-02-02 13:36:33 +0530

    Renew and Rekey Certificate Implementation

---
+++ src/main/java/com/czertainly/api/model/core/v2/ClientCertificateRekeyRequestDto.java
@@ -0,0 +1,43 @@
+package com.czertainly.api.model.core.v2;
+
+import com.czertainly.api.model.client.attribute.RequestAttributeDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Class representing a request to regenerate certificate
+ */
+@Data
+public class ClientCertificateRekeyRequestDto {
+
+    @Schema(
+            description = "True to replace renewed certificate in the associated locations",
+            defaultValue = "false"
+    )
+    public boolean replaceInLocations;
+    @Schema(
+            description = "Certificate sign request (PKCS#10) encoded as Base64 string. If not provided, CSR attributes will be used"
+    )
+    private String pkcs10;
+    @Schema(
+            description = "Key UUID",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
+    private UUID keyUuid;
+    @Schema(
+            description = "Token Profile UUID",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
+    private UUID tokenProfileUuid;
+    @Schema(
+            description = "CSR Attributes. If not provided, existing attributes will be used to generate the new CSR"
+    )
+    private List<RequestAttributeDto> csrAttributes;
+    @Schema(
+            description = "Signature Attributes. If not provided, existing attributes will be used to generate the new CSR"
+    )
+    private List<RequestAttributeDto> signatureAttributes;
+}
--- src/main/java/com/czertainly/api/model/core/v2/ClientCertificateRenewRequestDto.java
+++ src/main/java/com/czertainly/api/model/core/v2/ClientCertificateRenewRequestDto.java
@@ -19,28 +19,7 @@ public class ClientCertificateRenewRequestDto {
     )
     public boolean replaceInLocations;
     @Schema(
-            description = "Certificate sign request (PKCS#10) encoded as Base64 string. If not provided, CSR attributes will be used"
+            description = "Certificate sign request (PKCS#10) encoded as Base64 string. If not provided, Existing CSR will be used"
     )
     private String pkcs10;
-    @Schema(
-            description = "Use existing CSR",
-            defaultValue = "false"
-    )
-    private boolean useExistingCsr;
-    @Schema(
-            description = "Key UUID. Required to create new CSR. If not provided, Key from existing certificate will be used"
-    )
-    private UUID keyUuid;
-    @Schema(
-            description = "Token Profile UUID. Required if new CSR is to be generated and key is changed"
-    )
-    private UUID tokenProfileUuid;
-    @Schema(
-            description = "CSR Attributes. If not provided, existing attributes will be used to generate the new CSR"
-    )
-    private List<RequestAttributeDto> csrAttributes;
-    @Schema(
-            description = "Signature Attributes. If not provided, existing attributes will be used to generate the new CSR"
-    )
-    private List<RequestAttributeDto> signatureAttributes;
 }
