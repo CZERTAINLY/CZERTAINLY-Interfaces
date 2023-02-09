e6adb37899984e2549b09398f60e0345be46d394
3KeyPradeep <pradeep.saminathan@3key.company>
2023-02-09 12:44:11 +0530

    Update CertificateController.java

--- src/main/java/com/czertainly/api/interfaces/core/web/CertificateController.java
+++ src/main/java/com/czertainly/api/interfaces/core/web/CertificateController.java
@@ -183,7 +183,7 @@ public interface CertificateController {
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Certificate content retrieved"),
             @ApiResponse(responseCode = "422", description = "Unprocessible Entity", content = @Content(array = @ArraySchema(schema = @Schema(implementation = String.class)),
                     examples = {@ExampleObject(value = "[\"Error Message 1\",\"Error Message 2\"]")}))})
-    @RequestMapping(path = "/content", method = RequestMethod.GET, produces = {"application/json"})
+    @RequestMapping(path = "/content", method = RequestMethod.POST, consumes = {"application/json"}, produces = {"application/json"})
     List<CertificateContentDto> getCertificateContent(@io.swagger.v3.oas.annotations.parameters.RequestBody(
             description = "Certificate UUIDs", content = @Content(array = @ArraySchema(schema = @Schema(implementation = String.class)),
             examples = {@ExampleObject(value = "[\"c2f685d4-6a3e-11ec-90d6-0242ac120003\",\"b9b09548-a97c-4c6a-a06a-e4ee6fc2da98\"]")}))
