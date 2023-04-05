8c9bfe91c4a3385393afc9707537234d37df70ff
moro-lukasrejha <lukas.rejha@morosystems.cz>
2023-04-05 20:49:01 +0200

    CZERTAINLY-Core #319 Add search filters for discoveries inventory with support for pagination - implementation with tests

--- src/main/java/com/czertainly/api/model/core/discovery/DiscoveryStatus.java
+++ src/main/java/com/czertainly/api/model/core/discovery/DiscoveryStatus.java
@@ -2,6 +2,7 @@ package com.czertainly.api.model.core.discovery;
 
 import com.czertainly.api.exception.ValidationError;
 import com.czertainly.api.exception.ValidationException;
+import com.czertainly.api.model.connector.cryptography.enums.IAbstractSearchableEnum;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -9,7 +10,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Arrays;
 
 @Schema(enumAsRef = true)
-public enum DiscoveryStatus {
+public enum DiscoveryStatus implements IAbstractSearchableEnum {
 
     IN_PROGRESS("inProgress"),
     FAILED("failed"),
@@ -36,5 +37,10 @@ public enum DiscoveryStatus {
                 .findFirst()
                 .orElseThrow(() ->
                         new ValidationException(ValidationError.create("Unknown status {}", code)));
+    }
+
+    @Override
+    public String getEnumLabel() {
+        return this.code;
     }
 }
