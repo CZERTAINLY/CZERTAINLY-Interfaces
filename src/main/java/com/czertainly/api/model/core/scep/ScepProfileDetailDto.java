ea33190762d63a2a209f2b61f7b1c69af4f4f899
3keyroman <roman.cinkais@3key.company>
2023-04-07 17:25:23 +0200

    Update SCEP description

--- src/main/java/com/czertainly/api/model/core/scep/ScepProfileDetailDto.java
+++ src/main/java/com/czertainly/api/model/core/scep/ScepProfileDetailDto.java
@@ -10,29 +10,15 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-public class ScepProfileDetailDto extends NameAndUuidDto {
-    @Schema(description = "Enabled flag - true = enabled; false = disabled",
-            requiredMode = Schema.RequiredMode.REQUIRED)
-    private boolean enabled;
-    @Schema(description = "ACME Profile description", example = "Sample description")
-    private String description;
-    @Schema(description = "Enforce manual approval for all requests", requiredMode = Schema.RequiredMode.REQUIRED)
-    private boolean requireManualApproval;
+public class ScepProfileDetailDto extends ScepProfileDto {
     @Schema(description = "RA Profile")
     private RaProfileDto raProfile;
-    @Schema(description = "SCEP URL", example = "https://some-server.com/api/v1/protocols/acme/profile1/directory")
+    @Schema(description = "SCEP URL", example = "https://some-server.com/api/v1/protocols/scep/profile/pkiclient.exe")
     private String scepUrl;
     @Schema(description = "List of Attributes to issue a Certificate")
     private List<ResponseAttributeDto> issueCertificateAttributes;
     @Schema(description = "List of Custom Attributes")
     private List<ResponseAttributeDto> customAttributes;
-
     @Schema(description = "CA Certificate for the SCEP Profile")
     private CertificateDto caCertificate;
-    @Schema(description = "Include CA Certificate in the scep response", requiredMode = Schema.RequiredMode.REQUIRED)
-    private boolean includeCaCertificate;
-    @Schema(description = "Include CA Certificate Chain in the scep response", requiredMode = Schema.RequiredMode.REQUIRED)
-    private boolean includeCaCertificateChain;
-    @Schema(description = "Renew Threshold")
-    private Integer renewThreshold;
 }
