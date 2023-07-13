1f095851399db9c6f4c4e7cd0054fcc0e82851ac
David Maixner <david.maixner@morosystems.cz>
2023-07-13 09:09:11 +0200

    Add sending timestamp and target object identification to notification
    close #496

--- src/main/java/com/czertainly/api/model/client/notification/NotificationDto.java
+++ src/main/java/com/czertainly/api/model/client/notification/NotificationDto.java
@@ -1,9 +1,11 @@
 package com.czertainly.api.model.client.notification;
 
+import com.czertainly.api.model.core.auth.Resource;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -19,4 +21,13 @@ public class NotificationDto {
 
     @Schema(description = "Notification read date")
     private Date readAt;
+
+    @Schema(description = "Notification sent date", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Date sentAt;
+
+    @Schema(description = "Target object type")
+    private Resource targetObjectType;
+
+    @Schema(description = "Target object identification (UUID)")
+    private List<String> targetObjectIdentification;
 }
