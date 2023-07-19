c9e320a426880f037304e126beb94a23b7352142
moro-lukasrejha <122088314+moro-lukasrejha@users.noreply.github.com>
2023-07-19 10:28:52 +0200

    Add endpoint for listing user approvals (#284)

--- src/main/java/com/czertainly/api/interfaces/core/web/ApprovalController.java
+++ src/main/java/com/czertainly/api/interfaces/core/web/ApprovalController.java
@@ -4,12 +4,14 @@ import com.czertainly.api.exception.NotFoundException;
 import com.czertainly.api.exception.ValidationException;
 import com.czertainly.api.model.client.approval.ApprovalDetailDto;
 import com.czertainly.api.model.client.approval.ApprovalResponseDto;
+import com.czertainly.api.model.client.approval.ApprovalUserDto;
 import com.czertainly.api.model.client.approval.UserApprovalDto;
 import com.czertainly.api.model.common.AuthenticationServiceExceptionDto;
 import com.czertainly.api.model.common.ErrorMessageDto;
 import com.czertainly.api.model.core.scheduler.PaginationRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -54,7 +56,13 @@ public interface ApprovalController {
     @Operation(summary = "List of Approvals")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "List of all approvals")})
     @RequestMapping(method = RequestMethod.GET, produces = {"application/json"})
-    ApprovalResponseDto listApprovals(final PaginationRequestDto paginationRequestDto) throws ValidationException;
+    ApprovalResponseDto listApprovals(@RequestBody final PaginationRequestDto paginationRequestDto) throws ValidationException;
+
+    @Operation(summary = "List of User's Approvals")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "List of all approvals")})
+    @RequestMapping(method = RequestMethod.GET, path = "/user", produces = {"application/json"})
+    ApprovalResponseDto listUserApprovals(@RequestBody final PaginationRequestDto paginationRequestDto,
+            @Parameter(in = ParameterIn.QUERY, description = "Select if you want to list all history of approvals by user") ApprovalUserDto approvalUserDto) throws ValidationException;
 
     @Operation(summary = "Get Approval Detail")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Approval detail retrieved")})
@@ -64,26 +72,26 @@ public interface ApprovalController {
 
 
     @Operation(summary = "Approving of the Approval")
-    @ApiResponses(value = { @ApiResponse(responseCode = "204", description = "Approval approved")})
+    @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "Approval approved")})
     @RequestMapping(path = "/{uuid}/approve", method = RequestMethod.PATCH)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void approveApproval(@Parameter(description = "Approval UUID") @PathVariable String uuid) throws NotFoundException;
 
     @Operation(summary = "Rejecting of the Approval")
-    @ApiResponses(value = { @ApiResponse(responseCode = "204", description = "Approval rejected")})
+    @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "Approval rejected")})
     @RequestMapping(path = "/{uuid}/reject", method = RequestMethod.PATCH)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void rejectApproval(@Parameter(description = "Approval UUID") @PathVariable String uuid) throws NotFoundException;
 
     @Operation(summary = "Approving of Recipient of the Approval")
-    @ApiResponses(value = { @ApiResponse(responseCode = "204", description = "Approval Recipient approved")})
+    @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "Approval Recipient approved")})
     @RequestMapping(path = "/{uuid}/approveRecipient", method = RequestMethod.PATCH)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void approveApprovalRecipient(@Parameter(description = "Approval UUID") @PathVariable String uuid,
-            @RequestBody UserApprovalDto userApprovalDto) throws NotFoundException;
+                                  @RequestBody UserApprovalDto userApprovalDto) throws NotFoundException;
 
     @Operation(summary = "Rejecting of Recipient of the Approval")
-    @ApiResponses(value = { @ApiResponse(responseCode = "204", description = "Approval Recipient rejected")})
+    @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "Approval Recipient rejected")})
     @RequestMapping(path = "/{uuid}/rejectRecipient", method = RequestMethod.PATCH)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void rejectApprovalRecipient(@Parameter(description = "Approval UUID") @PathVariable String uuid,
--- src/main/java/com/czertainly/api/model/client/approval/ApprovalUserDto.java
+++ src/main/java/com/czertainly/api/model/client/approval/ApprovalUserDto.java
@@ -3,8 +3,8 @@ package com.czertainly.api.model.client.approval;
 import lombok.Data;
 
 @Data
-public class UserApprovalDto {
+public class ApprovalUserDto {
 
-    private String comment;
+    private boolean history;
 
 }
