1b1038be9c8471909b8ec956bc5b55e8e6508d09
dmaixner-moro <116064373+dmaixner-moro@users.noreply.github.com>
2023-07-20 12:41:49 +0200

    Add endpoints for management of notification instances (#286)

--- src/main/java/com/czertainly/api/model/connector/notification/NotificationRecipientDto.java
+++ src/main/java/com/czertainly/api/model/connector/notification/NotificationRecipientDto.java
@@ -2,7 +2,6 @@
 package com.czertainly.api.model.connector.notification;
 
 import com.czertainly.api.model.client.attribute.RequestAttributeDto;
-import com.czertainly.api.model.client.attribute.ResponseAttributeDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -16,9 +15,6 @@ public class NotificationRecipientDto {
     @Schema(description = "Recipient email", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String email;
 
-    @Schema(description = "Mapping attributes value for recipient", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    private List<RequestAttributeDto> mappingAttributes;
-
-    @Schema(description = "List of Custom Attributes", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    private List<ResponseAttributeDto> customAttributes;
+    @Schema(description = "Mapped attributes values for recipient", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private List<RequestAttributeDto> mappedAttributes;
 }
