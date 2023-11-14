eed3c33cc0a635e80f2748ca2d26e7f8e3e8e463
lubomirw <76479559+lubomirw@users.noreply.github.com>
2023-11-14 12:38:00 +0100

    Add identify endpoint to Authority certificate V2 interface (#325)

--- src/main/java/com/czertainly/api/model/connector/v2/CertificateDataResponseDto.java
+++ src/main/java/com/czertainly/api/model/connector/v2/CertificateDataResponseDto.java
@@ -3,6 +3,7 @@ package com.czertainly.api.model.connector.v2;
 import com.czertainly.api.model.common.attribute.v2.MetadataAttribute;
 import com.czertainly.api.model.core.certificate.CertificateType;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -11,6 +12,7 @@ import java.util.List;
 /**
  * Response containing signed certificate data
  */
+@Data
 public class CertificateDataResponseDto {
 
     @Schema(description = "Base64 encoded Certificate content",
@@ -18,12 +20,12 @@ public class CertificateDataResponseDto {
     private String certificateData;
 
     @Schema(description = "UUID of Certificate",
-            requiredMode = Schema.RequiredMode.REQUIRED)
+            requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String uuid;
 
     @Schema(
             description = "Metadata for the Certificate",
-            requiredMode = Schema.RequiredMode.REQUIRED
+            requiredMode = Schema.RequiredMode.NOT_REQUIRED
     )
     private List<MetadataAttribute> meta;
 
@@ -31,30 +33,6 @@ public class CertificateDataResponseDto {
             defaultValue = "X509",
             requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private CertificateType certificateType;
-
-    public String getCertificateData() {
-        return certificateData;
-    }
-
-    public void setCertificateData(String certificateData) {
-        this.certificateData = certificateData;
-    }
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public List<MetadataAttribute> getMeta() {
-        return meta;
-    }
-
-    public void setMeta(List<MetadataAttribute> meta) {
-        this.meta = meta;
-    }
 
     @Override
     public String toString() {
