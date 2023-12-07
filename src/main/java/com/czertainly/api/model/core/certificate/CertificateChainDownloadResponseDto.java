5d0b481bf46c0758abba27085da7b2eaa9f2baed
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2023-12-07 13:21:19 +0100

    Rewrite dto properties description

--- src/main/java/com/czertainly/api/model/core/certificate/CertificateChainDownloadResponseDto.java
+++ src/main/java/com/czertainly/api/model/core/certificate/CertificateChainDownloadResponseDto.java
@@ -14,22 +14,4 @@ public class CertificateChainDownloadResponseDto extends CertificateDownloadResp
             requiredMode = Schema.RequiredMode.REQUIRED
     )
     private boolean completeChain;
-
-    @Schema(
-            description = "Format of the downloaded chain",
-            requiredMode = Schema.RequiredMode.REQUIRED
-    )
-    private CertificateFormat format;
-
-    @Schema(
-            description = "Encoding of the downloaded certificate chain",
-            requiredMode = Schema.RequiredMode.REQUIRED
-    )
-    private CertificateFormatEncoding encoding;
-
-    @Schema(
-            description = "Base64 encoded certificate chain in the specified format",
-            requiredMode = Schema.RequiredMode.REQUIRED
-    )
-    private String content;
 }
--- src/main/java/com/czertainly/api/model/core/certificate/CertificateDownloadResponseDto.java
+++ src/main/java/com/czertainly/api/model/core/certificate/CertificateDownloadResponseDto.java
@@ -9,19 +9,19 @@ import java.util.List;
 public class CertificateDownloadResponseDto {
 
     @Schema(
-            description = "Format of the downloaded certificate",
+            description = "Format of the downloaded content",
             requiredMode = Schema.RequiredMode.REQUIRED
     )
     private CertificateFormat format;
 
     @Schema(
-            description = "Encoding of the downloaded certificate",
+            description = "Encoding of the downloaded content",
             requiredMode = Schema.RequiredMode.REQUIRED
     )
     private CertificateFormatEncoding encoding;
 
     @Schema(
-            description = "Base64 encoded certificate in the specified format",
+            description = "Base64 encoded content in the specified format and encoding",
             requiredMode = Schema.RequiredMode.REQUIRED
     )
     private String content;
