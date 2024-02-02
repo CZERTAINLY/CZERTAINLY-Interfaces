2fec9d29eac2be432d111f12db68e44edc45b156
lubomirw <76479559+lubomirw@users.noreply.github.com>
2024-02-02 11:02:08 +0100

    Update search field identifier description to inform how to get identifier (#346)

--- src/main/java/com/czertainly/api/model/client/certificate/SearchFilterRequestDto.java
+++ src/main/java/com/czertainly/api/model/client/certificate/SearchFilterRequestDto.java
@@ -8,13 +8,15 @@ import java.io.Serializable;
 
 public class SearchFilterRequestDto {
 
-    @Schema(description = "Group to search", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(description = "Field group of search filter", requiredMode = Schema.RequiredMode.REQUIRED)
     private SearchGroup searchGroup;
 
-    @Schema(description = "Field to search", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(description = "Field identifier of search filter. List of available fields with their identifiers can be retrieved from corresponding endpoint " +
+            "`GET /v1/{resource}/search`, e.g.: [**GET /v1/certificates/search**](../core-certificate/#tag/Certificate-Inventory/operation/getSearchableFieldInformation)",
+            requiredMode = Schema.RequiredMode.REQUIRED)
     private String fieldIdentifier;
 
-    @Schema(description = "Condition for the search", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(description = "Condition for the search filter", requiredMode = Schema.RequiredMode.REQUIRED)
     private SearchCondition condition;
 
     @Schema(description = "Value to match")
