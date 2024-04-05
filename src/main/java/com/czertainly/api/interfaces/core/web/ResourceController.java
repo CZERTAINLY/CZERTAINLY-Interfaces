a3c24bfca1807575238f05946ac89da294ac9b5f
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2024-04-05 16:38:52 +0200

    Update ResourceController.java

--- src/main/java/com/czertainly/api/interfaces/core/web/ResourceController.java
+++ src/main/java/com/czertainly/api/interfaces/core/web/ResourceController.java
@@ -4,7 +4,6 @@ package com.czertainly.api.interfaces.core.web;
 import com.czertainly.api.exception.NotFoundException;
 import com.czertainly.api.model.common.AuthenticationServiceExceptionDto;
 import com.czertainly.api.model.core.auth.Resource;
-import com.czertainly.api.model.core.auth.RoleDetailDto;
 import com.czertainly.api.model.core.search.SearchFieldDataByGroupDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
