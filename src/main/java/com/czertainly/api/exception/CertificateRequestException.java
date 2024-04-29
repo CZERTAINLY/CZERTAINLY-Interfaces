856c56695eed9cf3947b22d6be02c1dd0b6d02fe
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2024-04-29 10:32:47 +0200

    Add CertificateRequestException

---
+++ src/main/java/com/czertainly/api/exception/CertificateRequestException.java
@@ -0,0 +1,8 @@
+package com.czertainly.api.exception;
+
+public class CertificateRequestException extends Exception {
+
+    public CertificateRequestException(String message) {
+        super(message);
+    }
+}
