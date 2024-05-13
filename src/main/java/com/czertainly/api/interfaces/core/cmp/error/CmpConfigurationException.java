ccde6a7fee8cf9f22a8a324508c9addc8af8f58b
Tomas Cejka <tomas.cejka@cloudfield.cz>
2024-05-13 10:48:26 +0200

    cmp-api - feat: add cmp api (based on code-review prompts)

---
+++ src/main/java/com/czertainly/api/interfaces/core/cmp/error/CmpConfigurationException.java
@@ -0,0 +1,19 @@
+package com.czertainly.api.interfaces.core.cmp.error;
+
+import org.bouncycastle.asn1.ASN1OctetString;
+
+/**
+ * This exception is created if a configuration (scope: cmp request/response)
+ * is failed or in corrupted state.
+ */
+public class CmpConfigurationException extends CmpBaseException {
+
+    public CmpConfigurationException(int failureInfo, String errorDetails) {
+        super(null, failureInfo, errorDetails, null);
+    }
+
+    public CmpConfigurationException(ASN1OctetString tid, int failureInfo, String errorDetails) {
+        super(tid, failureInfo, errorDetails, null);
+    }
+
+}
