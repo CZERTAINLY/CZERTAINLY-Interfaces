b233951bb081a99c24254cf201780a06070d00dd
lubomirw <lubomir@wassermann.sk>
2024-05-28 14:34:18 +0200

    Fix switching of name and description in @Schema in TriggerHistoryRecordDto

--- src/main/java/com/czertainly/api/model/core/workflows/TriggerHistoryRecordDto.java
+++ src/main/java/com/czertainly/api/model/core/workflows/TriggerHistoryRecordDto.java
@@ -5,12 +5,12 @@ import lombok.Data;
 
 @Data
 public class TriggerHistoryRecordDto {
-    @Schema(name = "Message with cause of action/condition failure.", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(description = "Message with cause of action/condition failure.", requiredMode = Schema.RequiredMode.REQUIRED)
     private String message;
 
-    @Schema(name = "Condition that is referenced by history record")
+    @Schema(description = "Condition that is referenced by history record")
     private ConditionDto condition;
 
-    @Schema(name = "Execution that is referenced by history record")
+    @Schema(description = "Execution that is referenced by history record")
     private ExecutionDto execution;
 }
