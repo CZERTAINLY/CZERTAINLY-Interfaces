deb3d2feffe5954b5a9a7da6146ef240aac58d7e
Tomáš Čejka <44152+tomascejka@users.noreply.github.com>
2024-06-07 11:30:50 +0200

    Add new constructor to CmpCrmfValidationException (#391)

--- src/main/java/com/czertainly/api/interfaces/core/cmp/error/CmpCrmfValidationException.java
+++ src/main/java/com/czertainly/api/interfaces/core/cmp/error/CmpCrmfValidationException.java
@@ -5,7 +5,7 @@ import org.bouncycastle.asn1.ASN1OctetString;
 import org.bouncycastle.asn1.cmp.PKIBody;
 
 /**
- * This exception is created if a enrollment (scope: cmp request, types: ir, cr, kur)
+ * This exception is created if an enrollment (scope: cmp request, types: ir, cr, kur)
  * is failed or in corrupted state.
  */
 public class CmpCrmfValidationException extends CmpProcessingException {
@@ -13,9 +13,12 @@ public class CmpCrmfValidationException extends CmpProcessingException {
     private final int typeOfRequestEnrollment;
 
     /**
+     * Create exception with additional information about failure
+     *
+     * @param tid                     transaction id
      * @param typeOfRequestEnrollment PKI message type of enrollment request, {@link PKIBody#getType()}
-     * @param failureInfo    information about error message (CMP level)
-     * @param errorDetails   description of some details related to the error
+     * @param failureInfo             information about error message (CMP level)
+     * @param errorDetails            description of some details related to the error
      */
     public CmpCrmfValidationException(
             ASN1OctetString tid, int typeOfRequestEnrollment, int failureInfo, String errorDetails) {
@@ -24,12 +27,27 @@ public class CmpCrmfValidationException extends CmpProcessingException {
     }
 
     /**
+     * Create exception with additional information about failure (implementation level)
+     *
+     * @param tid                     transaction id
+     * @param typeOfRequestEnrollment PKI message type of enrollment request, {@link PKIBody#getType()}
+     * @param failureInfo             information about error message (CMP level)
+     * @param implFailureInfo         information about error message (implementation level)
+     */
+    public CmpCrmfValidationException(
+            ASN1OctetString tid, int typeOfRequestEnrollment, int failureInfo, ImplFailureInfo implFailureInfo) {
+        super(tid, failureInfo, implFailureInfo, null);
+        this.typeOfRequestEnrollment = typeOfRequestEnrollment;
+    }
+
+    /**
      * Prepare response type from request type (using: requestType+1)
+     *
      * @return create {@link PKIBody} for response message
      */
     @Override
     public PKIBody toPKIBody() {
-        return PkiMessageError.generateCrmfErrorBody(this.typeOfRequestEnrollment+1,
+        return PkiMessageError.generateCrmfErrorBody(this.typeOfRequestEnrollment + 1,
                 failureInfo, errorDetails);
     }
 }
