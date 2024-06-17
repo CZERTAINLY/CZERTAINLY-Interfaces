042dfc8a201ec0194a561b60781f758c881768e1
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2024-06-17 10:14:42 +0200

    Create Protocol.java

---
+++ src/main/java/com/czertainly/api/model/core/enums/Protocol.java
@@ -0,0 +1,35 @@
+package com.czertainly.api.model.core.enums;
+
+import com.czertainly.api.model.common.enums.IPlatformEnum;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum Protocol implements IPlatformEnum {
+
+    ACME("acme", "ACME Protocol"),
+    SCEP("scep", "SCEP Protocol")
+    ;
+
+    private final String code;
+    private final String label;
+
+   Protocol(String code, String label) {
+        this.code = code;
+        this.label = label;
+    }
+
+    @Override
+    @JsonValue
+    public String getCode() {
+        return this.code;
+    }
+
+    @Override
+    public String getLabel() {
+        return this.label;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
