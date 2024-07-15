75daeda516362e8e30164da68e8a3ee224d18cb7
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2024-07-15 10:43:50 +0200

    Add CertificateProtocol enum (#394)

---
+++ src/main/java/com/czertainly/api/model/core/enums/CertificateProtocol.java
@@ -0,0 +1,36 @@
+package com.czertainly.api.model.core.enums;
+
+import com.czertainly.api.model.common.enums.IPlatformEnum;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum CertificateProtocol implements IPlatformEnum {
+
+    ACME("acme", "ACME Protocol"),
+    SCEP("scep", "SCEP Protocol"),
+    CMP("cmp", "CMP Protocol")
+    ;
+
+    private final String code;
+    private final String label;
+
+   CertificateProtocol(String code, String label) {
+        this.code = code;
+        this.label = label;
+    }
+
+    @Override
+    @JsonValue
+    public String getCode() {
+        return this.code;
+    }
+
+    @Override
+    public String getLabel() {
+        return this.label;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
