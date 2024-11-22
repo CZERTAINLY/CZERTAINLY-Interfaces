d6b53a03e50a963c35035928a29759102b03d2b3
lubomirw <lubomir@wassermann.sk>
2024-11-22 10:00:52 +0100

    Format code

--- src/main/java/com/czertainly/api/interfaces/core/web/SettingController.java
+++ src/main/java/com/czertainly/api/interfaces/core/web/SettingController.java
@@ -21,97 +21,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/settings")
 @Tag(name = "Settings", description = "Settings API")
-@ApiResponses(
-        value = {
-                @ApiResponse(
-                        responseCode = "400",
-                        description = "Bad Request",
-                        content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))
-                ),
-                @ApiResponse(
-                        responseCode = "401",
-                        description = "Unauthorized",
-                        content = @Content(schema = @Schema())
-                ),
-                @ApiResponse(
-                        responseCode = "403",
-                        description = "Forbidden",
-                        content = @Content(schema = @Schema(implementation = AuthenticationServiceExceptionDto.class))
-                ),
-                @ApiResponse(
-                        responseCode = "404",
-                        description = "Not Found",
-                        content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))
-                ),
-                @ApiResponse(
-                        responseCode = "500",
-                        description = "Internal Server Error",
-                        content = @Content
-                )
-        })
+@ApiResponses(value = {@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))), @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema())), @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(implementation = AuthenticationServiceExceptionDto.class))), @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))), @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)})
 public interface SettingController {
 
-    @Operation(
-            summary = "Get platform settings"
-    )
-    @ApiResponses(
-            value = {
-                    @ApiResponse(responseCode = "200", description = "Platform settings retrieved")
-            })
-    @RequestMapping(
-            method = RequestMethod.GET,
-            path = "/platform",
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @Operation(summary = "Get platform settings")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Platform settings retrieved")})
+    @GetMapping(path = "/platform", produces = MediaType.APPLICATION_JSON_VALUE)
     PlatformSettingsDto getPlatformSettings();
 
-    @Operation(
-            summary = "Update platform setting"
-    )
-    @ApiResponses(
-            value = {
-                    @ApiResponse(responseCode = "204", description = "Setting updated")
-            })
-    @RequestMapping(
-            method = RequestMethod.PUT,
-            path = "/platform",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    void updatePlatformSettings(
-            @RequestBody PlatformSettingsDto platformSettingsDto
-    );
+    @Operation(summary = "Update platform setting")
+    @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "Setting updated")})
+    @PutMapping(path = "/platform", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    void updatePlatformSettings(@RequestBody PlatformSettingsDto platformSettingsDto);
 
-    @Operation(
-            summary = "Get notification settings"
-    )
-    @ApiResponses(
-            value = {
-                    @ApiResponse(responseCode = "200", description = "Notification settings retrieved")
-            })
-    @RequestMapping(
-            method = RequestMethod.GET,
-            path = "/notifications",
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @Operation(summary = "Get notification settings")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Notification settings retrieved")})
+    @GetMapping(path = "/notifications", produces = MediaType.APPLICATION_JSON_VALUE)
     NotificationSettingsDto getNotificationsSettings();
 
-    @Operation(
-            summary = "Update notifications setting"
-    )
-    @ApiResponses(
-            value = {
-                    @ApiResponse(responseCode = "204", description = "Setting updated")
-            })
-    @RequestMapping(
-            method = RequestMethod.PUT,
-            path = "/notifications",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    void updateNotificationsSettings(
-            @RequestBody NotificationSettingsDto notificationSettingsDto
-    );
+    @Operation(summary = "Update notifications setting")
+    @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "Setting updated")})
+    @PutMapping(path = "/notifications", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    void updateNotificationsSettings(@RequestBody NotificationSettingsDto notificationSettingsDto);
 
     @Operation(summary = "Get OAuth2 Provider settings")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OAuth2 Provider settings retrieved")})
@@ -121,7 +52,7 @@ public interface SettingController {
     @Operation(summary = "Update OAuth2 Provider settings")
     @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "Setting updated")})
     @PutMapping(path = "/oauth2Providers/{providerName}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    void updateOAuth2ProviderSettings(@Parameter(description = "OAuth2 Provider Name") @PathVariable String providerName,@Valid @RequestBody OAuth2ProviderSettings oauth2SettingsDto);
+    void updateOAuth2ProviderSettings(@Parameter(description = "OAuth2 Provider Name") @PathVariable String providerName, @Valid @RequestBody OAuth2ProviderSettings oauth2SettingsDto);
 
     @Operation(summary = "Retrieve list of configured OAuth2 Providers")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OAuth2 Providers retrieved")})
@@ -133,23 +64,13 @@ public interface SettingController {
     @DeleteMapping(path = "/oauth2Providers/{providerName}")
     void removeOAuth2Provider(@Parameter(description = "OAuth2 Provider Name") @PathVariable String providerName);
 
-    @Operation(
-            summary = "Get logging settings"
-    )
-    @ApiResponses(
-            value = {
-                    @ApiResponse(responseCode = "200", description = "Logging settings retrieved")
-            })
+    @Operation(summary = "Get logging settings")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Logging settings retrieved")})
     @GetMapping(path = "/logging", produces = MediaType.APPLICATION_JSON_VALUE)
     LoggingSettingsDto getLoggingSettings();
 
-    @Operation(
-            summary = "Update logging setting"
-    )
-    @ApiResponses(
-            value = {
-                    @ApiResponse(responseCode = "200", description = "Setting updated")
-            })
+    @Operation(summary = "Update logging setting")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Setting updated")})
     @PutMapping(path = "/logging", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     void updateLoggingSettings(@RequestBody LoggingSettingsDto loggingSettingsDto);
 }
