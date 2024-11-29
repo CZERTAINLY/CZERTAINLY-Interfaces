0c59e49821ea67c0075562152621722a9cdb0e46
lubomirw <lubomir@wassermann.sk>
2024-11-29 11:25:08 +0100

    OAuth2Providers list in AuthenticationSettingsUpdateDto should be optional

--- src/main/java/com/czertainly/api/model/core/settings/authentication/AuthenticationSettingsUpdateDto.java
+++ src/main/java/com/czertainly/api/model/core/settings/authentication/AuthenticationSettingsUpdateDto.java
@@ -1,6 +1,5 @@
 package com.czertainly.api.model.core.settings.authentication;
 
-import com.czertainly.api.model.core.settings.SettingsDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -8,12 +7,12 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-public class AuthenticationSettingsUpdateDto implements SettingsDto {
+public class AuthenticationSettingsUpdateDto {
 
     @NotNull
     @Schema(description = "Disable using localhost user for requests from localhost", requiredMode = Schema.RequiredMode.REQUIRED)
     private boolean disableLocalhostUser = false;
 
-    @Schema(description = "List of OAuth2 providers settings to be registered. If null, keep providers as they are, otherwise replace with providers from list", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(description = "List of OAuth2 providers settings to be registered. If null, keep providers as they are, otherwise replace with providers from list")
     private List<OAuth2ProviderSettingsDto> oAuth2Providers;
 }
