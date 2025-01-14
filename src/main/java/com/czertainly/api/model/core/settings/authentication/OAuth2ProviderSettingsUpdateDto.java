d8a887bf9eca678bdcf4826f2c318279a9a9974d
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-01-14 11:56:38 +0100

    Update OAuth2ProviderSettingsUpdateDto.java

--- src/main/java/com/czertainly/api/model/core/settings/authentication/OAuth2ProviderSettingsUpdateDto.java
+++ src/main/java/com/czertainly/api/model/core/settings/authentication/OAuth2ProviderSettingsUpdateDto.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class OAuth2ProviderSettingsUpdateDto implements Serializable {
 
     @URL
-    @Schema(description = "URL of issuer issuing authentication tokens. If provided, authentication via JWT token is enabled for this provider.")
+    @Schema(description = "URL of issuer issuing authentication tokens. If provided, authentication via JWT token is enabled for this provider.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String issuerUrl;
 
     @Schema(description = "The client ID used to identify the client application during the authorization process.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
@@ -35,10 +35,10 @@ public class OAuth2ProviderSettingsUpdateDto implements Serializable {
     @Schema(description = "The URL where the JSON Web Key Set (JWKS) containing the public keys used to verify JWT tokens can be retrieved.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String jwkSetUrl;
 
-    @Schema(description = "Base64 encoded JWK Set, provided in case JWK Set URL is not available")
+    @Schema(description = "Base64 encoded JWK Set, provided in case JWK Set URL is not available", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String jwkSet;
 
-    @Schema(description = " The list of scopes that define the access levels and permissions requested by the client application.")
+    @Schema(description = " The list of scopes that define the access levels and permissions requested by the client application.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private List<String> scope = new ArrayList<>();
 
     @URL
@@ -50,16 +50,16 @@ public class OAuth2ProviderSettingsUpdateDto implements Serializable {
     private String postLogoutUrl;
 
     @URL
-    @Schema(description = "The URL containing information about user.")
+    @Schema(description = "The URL containing information about user.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String userInfoUrl;
 
-    @Schema(description = "A list of expected audiences for validating the issued tokens, used to match the intended recipients of the tokens.")
+    @Schema(description = "A list of expected audiences for validating the issued tokens, used to match the intended recipients of the tokens.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private List<String> audiences = new ArrayList<>();
 
-    @Schema(description = "The allowed time skew, in seconds, for token validation. This accounts for clock differences between systems. Default value is 30 seconds.")
+    @Schema(description = "The allowed time skew, in seconds, for token validation. This accounts for clock differences between systems. Default value is 30 seconds.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private int skew = 30;
 
-    @Schema(description = "Duration in seconds after which will inactive user's session be terminated.", defaultValue = "15m")
+    @Schema(description = "Duration in seconds after which will inactive user's session be terminated.", defaultValue = "15m", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private int sessionMaxInactiveInterval = 15 * 60;
 
 }
