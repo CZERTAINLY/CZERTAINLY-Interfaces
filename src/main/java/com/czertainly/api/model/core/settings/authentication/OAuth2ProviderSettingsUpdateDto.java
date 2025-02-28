d576d36652e936478e24146a5f7e1e07254832cf
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-02-28 17:07:14 +0100

    Use regex in URL annotation

--- src/main/java/com/czertainly/api/model/core/settings/authentication/OAuth2ProviderSettingsUpdateDto.java
+++ src/main/java/com/czertainly/api/model/core/settings/authentication/OAuth2ProviderSettingsUpdateDto.java
@@ -2,7 +2,6 @@ package com.czertainly.api.model.core.settings.authentication;
 
 import com.czertainly.api.model.core.logging.Sensitive;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.URL;
 
@@ -13,8 +12,7 @@ import java.util.List;
 @Data
 public class OAuth2ProviderSettingsUpdateDto implements Serializable {
 
-    @URL
-    @Pattern(regexp = ".+", message = "URL cannot be empty")
+    @URL(regexp = "^(http).*")
     @Schema(description = "URL of issuer issuing authentication tokens. If provided, authentication via JWT token is enabled for this provider.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String issuerUrl;
 
@@ -25,18 +23,15 @@ public class OAuth2ProviderSettingsUpdateDto implements Serializable {
     @Schema(description = "The client secret used by the client application to authenticate with the authorization server.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String clientSecret;
 
-    @URL
-    @Pattern(regexp = ".+", message = "URL cannot be empty")
+    @URL(regexp = "^(http).*")
     @Schema(description = "The URL where the authorization server redirects the user for login and authorization.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String authorizationUrl;
 
-    @URL
-    @Pattern(regexp = ".+", message = "URL cannot be empty")
+    @URL(regexp = "^(http).*")
     @Schema(description = "The URl used to exchange the authorization code or credentials for an access token.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String tokenUrl;
 
-    @URL
-    @Pattern(regexp = ".+", message = "URL cannot be empty")
+    @URL(regexp = "^(http).*")
     @Schema(description = "The URL where the JSON Web Key Set (JWKS) containing the public keys used to verify JWT tokens can be retrieved.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String jwkSetUrl;
 
@@ -46,18 +41,15 @@ public class OAuth2ProviderSettingsUpdateDto implements Serializable {
     @Schema(description = " The list of scopes that define the access levels and permissions requested by the client application.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private List<String> scope = new ArrayList<>();
 
-    @URL
-    @Pattern(regexp = ".+", message = "URL cannot be empty")
+    @URL(regexp = "^(http).*")
     @Schema(description = "URL to end session on provider side.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String logoutUrl;
 
-    @URL
-    @Pattern(regexp = ".+", message = "URL cannot be empty")
+    @URL(regexp = "^(http).*")
     @Schema(description = "URL that user will be redirected after logout from application.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String postLogoutUrl;
 
-    @URL
-    @Pattern(regexp = ".+", message = "URL cannot be empty")
+    @URL(regexp = "^(http).*")
     @Schema(description = "The URL containing information about user.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String userInfoUrl;
 
