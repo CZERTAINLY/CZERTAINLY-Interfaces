dc4df92f212c0db558a454174ed2329410e10b0e
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-02-28 16:58:02 +0100

    Remove imports

--- src/main/java/com/czertainly/api/model/core/settings/authentication/OAuth2ProviderSettingsUpdateDto.java
+++ src/main/java/com/czertainly/api/model/core/settings/authentication/OAuth2ProviderSettingsUpdateDto.java
@@ -2,8 +2,6 @@ package com.czertainly.api.model.core.settings.authentication;
 
 import com.czertainly.api.model.core.logging.Sensitive;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.URL;
