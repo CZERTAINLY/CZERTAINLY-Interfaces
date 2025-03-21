5b6a47306cd32f85989833e14b7e6199d99b105c
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-03-21 16:02:46 +0100

    Refactor certificate validation settings dtos

---
+++ src/main/java/com/czertainly/api/model/core/settings/CertificateValidationSettingsUpdateDto.java
@@ -0,0 +1,23 @@
+package com.czertainly.api.model.core.settings;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class CertificateValidationSettingsUpdateDto implements Serializable {
+
+    @Schema(description = "Indicator whether validation of certificates should be enabled", requiredMode = Schema.RequiredMode.REQUIRED, defaultValue = "true")
+    private boolean enabled = true;
+
+    @Schema(description = "Frequency of validation of certificates in days", requiredMode = Schema.RequiredMode.NOT_REQUIRED, defaultValue = "1", minimum = "1")
+    @Positive
+    private Integer frequency = 1;
+
+    @Schema(description = "How many days before expiration should certificate validation status change to Expiring", requiredMode = Schema.RequiredMode.NOT_REQUIRED, defaultValue = "30", minimum = "1")
+    @Positive
+    private Integer expiringThreshold = 30;
+
+}
---
+++ src/main/java/com/czertainly/api/model/core/settings/PlatformSettingsUpdateDto.java
@@ -0,0 +1,18 @@
+package com.czertainly.api.model.core.settings;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import lombok.Data;
+
+@Data
+public class PlatformSettingsUpdateDto implements SettingsDto {
+
+    @Valid
+    @Schema(description = "Utils settings of the platform")
+    private UtilsSettingsDto utils;
+
+    @Valid
+    @Schema(description = "Settings applicable to all certificates in inventory by default")
+    private CertificateSettingsUpdateDto certificates;
+
+}
