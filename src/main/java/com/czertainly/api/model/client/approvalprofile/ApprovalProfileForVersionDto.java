77f3c2d8ef7098b33657b888d98c4feb46a727b0
lubomirw <76479559+lubomirw@users.noreply.github.com>
2025-04-08 13:58:48 +0200

    Fix query params wrapped as object to be generated as separate params in OpenAPI (#464)

--- src/main/java/com/czertainly/api/model/client/approvalprofile/ApprovalProfileForVersionDto.java
+++ src/main/java/com/czertainly/api/model/client/approvalprofile/ApprovalProfileForVersionDto.java
@@ -1,8 +1,10 @@
 package com.czertainly.api.model.client.approvalprofile;
 
 import lombok.Data;
+import org.springdoc.core.annotations.ParameterObject;
 
 @Data
+@ParameterObject
 public class ApprovalProfileForVersionDto {
 
     private Integer version;
