6520c262bbff3683b87fceaee6f076e10c6c4522
Ľubomír Wassermann <76479559+lubomirw@users.noreply.github.com>
2025-05-20 08:32:22 +0200

    Fix constructors of event settings DTOs

--- src/main/java/com/czertainly/api/model/core/settings/EventSettingsDto.java
+++ src/main/java/com/czertainly/api/model/core/settings/EventSettingsDto.java
@@ -5,10 +5,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.*;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class EventSettingsDto {
 
--- src/main/java/com/czertainly/api/model/core/settings/EventsSettingsDto.java
+++ src/main/java/com/czertainly/api/model/core/settings/EventsSettingsDto.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.EnumMap;
 import java.util.List;
@@ -12,10 +13,11 @@ import java.util.Map;
 import java.util.UUID;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class EventsSettingsDto implements SettingsDto {
 
     @NotNull
     @Schema(description = "Map of triggers associated to event where key is resource event enum", requiredMode = Schema.RequiredMode.REQUIRED)
-    private Map<ResourceEvent, List<UUID>> notificationsMapping = new EnumMap<>(ResourceEvent.class);
+    private Map<ResourceEvent, List<UUID>> eventsMapping = new EnumMap<>(ResourceEvent.class);
 }
