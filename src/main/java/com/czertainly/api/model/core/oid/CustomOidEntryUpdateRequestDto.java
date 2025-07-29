a17fd2886775fed0e65040d3167334de614c6862
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-07-29 13:05:06 +0200

    Update CustomOidEntryUpdateRequestDto.java

--- src/main/java/com/czertainly/api/model/core/oid/CustomOidEntryUpdateRequestDto.java
+++ src/main/java/com/czertainly/api/model/core/oid/CustomOidEntryUpdateRequestDto.java
@@ -3,7 +3,6 @@ package com.czertainly.api.model.core.oid;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
