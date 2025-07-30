5eb5aa8a40f9594986ec6a71f05ea55ea46d5736
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-07-30 13:46:59 +0200

    Update CustomOidEntryDetailResponseDto.java

--- src/main/java/com/czertainly/api/model/core/oid/CustomOidEntryDetailResponseDto.java
+++ src/main/java/com/czertainly/api/model/core/oid/CustomOidEntryDetailResponseDto.java
@@ -1,6 +1,5 @@
 package com.czertainly.api.model.core.oid;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
