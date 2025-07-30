655c9cf00bdae439a680d4753de5cde08c3503d5
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-07-30 14:58:01 +0200

    Remove duplicate category in additional properties of Custom OIDs (#510)

--- src/main/java/com/czertainly/api/model/core/oid/AdditionalOidPropertiesDto.java
+++ src/main/java/com/czertainly/api/model/core/oid/AdditionalOidPropertiesDto.java
@@ -1,18 +1,15 @@
 package com.czertainly.api.model.core.oid;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.io.Serializable;
 
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.PROPERTY,
-        property = "type",
-        visible = true
+@Schema(
+        description = "Additional properties of custom OID entry",
+        type = "object",
+        subTypes = {
+                RdnAttributeTypeOidPropertiesDto.class
+        }
 )
-@JsonSubTypes({
-        @JsonSubTypes.Type(value = RdnAttributeTypeOidPropertiesDto.class, name = OidCategory.Codes.RDN_ATTRIBUTE_TYPE)
-})
 public interface AdditionalOidPropertiesDto extends Serializable {
 }
