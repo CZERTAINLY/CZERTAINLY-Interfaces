eb78924c3c40f866a6e7a7e43184272f3cad2b14
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-07-30 13:43:25 +0200

    Do not duplicate category in additional properties

--- src/main/java/com/czertainly/api/model/core/oid/CustomOidEntryDetailResponseDto.java
+++ src/main/java/com/czertainly/api/model/core/oid/CustomOidEntryDetailResponseDto.java
@@ -1,4 +1,6 @@
 package com.czertainly.api.model.core.oid;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -9,6 +11,6 @@ import java.io.Serializable;
 @Data
 public class CustomOidEntryDetailResponseDto extends CustomOidEntryResponseDto implements Serializable {
 
-    @Schema(description = "Additional properties depending on the OID category", requiredMode = Schema.RequiredMode.NOT_REQUIRED )
+    @Schema(description = "Additional properties depending on the OID category", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private AdditionalOidPropertiesDto additionalProperties;
 }
