a35aeeef9fa7562f09a9fd3b022ca6c5f979027d
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-08-01 10:38:36 +0200

    Add query parameter for archived certs

--- src/main/java/com/czertainly/api/interfaces/core/web/StatisticsController.java
+++ src/main/java/com/czertainly/api/interfaces/core/web/StatisticsController.java
@@ -3,17 +3,20 @@ package com.czertainly.api.interfaces.core.web;
 import com.czertainly.api.interfaces.AuthProtectedController;
 import com.czertainly.api.model.client.dashboard.StatisticsDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/v1/statistics")
 @Tag(name = "Statistics/Dashboard", description = "Statistics/Dashboard API")
 public interface StatisticsController extends AuthProtectedController {
-	@Operation(summary = "Get Dashboard/Statistics Details")
-	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Details retrieved")})
-	@GetMapping(produces = {"application/json"})
-	public StatisticsDto getStatistics();
+
+    @Operation(summary = "Get Dashboard/Statistics Details")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Details retrieved")})
+    @GetMapping(produces = {"application/json"})
+    StatisticsDto getStatistics(@Parameter(description = "Include archived certificates in the statistics") @RequestParam(value = "includeArchived") boolean includeArchived);
 }
