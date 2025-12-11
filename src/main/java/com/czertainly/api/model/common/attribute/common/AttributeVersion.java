172c990c45028510f195534cfd9393a0fa61d0e1
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-12-11 09:16:05 +0100

    Update AttributeVersion.java

--- src/main/java/com/czertainly/api/model/common/attribute/common/AttributeVersion.java
+++ src/main/java/com/czertainly/api/model/common/attribute/common/AttributeVersion.java
@@ -3,17 +3,22 @@ package com.czertainly.api.model.common.attribute.common;
 import com.czertainly.api.model.common.enums.IPlatformEnum;
 import com.fasterxml.jackson.annotation.JsonValue;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
 
 @Schema(enumAsRef = true)
 public enum AttributeVersion implements IPlatformEnum {
 
-    V2(Codes.V2),
-    V3(Codes.V3);
+    V2(Codes.V2, 2),
+    V3(Codes.V3, 3);
 
     private final String code;
 
-    AttributeVersion(String code) {
+    @Getter
+    private final int anInt;
+
+    AttributeVersion(String code, int anInt) {
         this.code = code;
+        this.anInt = anInt;
     }
 
     @Override
@@ -33,6 +38,7 @@ public enum AttributeVersion implements IPlatformEnum {
     }
 
     public static class Codes {
+        private Codes(){}
         public static final String V2 = "v2";
         public static final String V3 = "v3";
     }
