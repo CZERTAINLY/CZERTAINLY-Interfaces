634110e80efab6f6350e983b627fa2cca94ddd67
lubomirw <76479559+lubomirw@users.noreply.github.com>
2026-02-12 09:37:07 +0100

    Update build info schema description

--- src/main/java/com/czertainly/api/model/core/info/CoreInfoResponseDto.java
+++ src/main/java/com/czertainly/api/model/core/info/CoreInfoResponseDto.java
@@ -9,7 +9,7 @@ public class CoreInfoResponseDto {
     @Schema(description = "ILM Application information", requiredMode = Schema.RequiredMode.REQUIRED)
     private AppInfoDto app;
 
-    @Schema(description = "ILM Application build information. Present only in SNAPSHOT (dev) versions", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @Schema(description = "ILM Application build information. May be omitted depending on deployment configuration", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private BuildInfoDto build;
 
     @Schema(description = "Database information", requiredMode = Schema.RequiredMode.REQUIRED)
