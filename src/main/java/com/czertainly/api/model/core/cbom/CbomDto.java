fdf459a2f94613efa643e32299063a18e44d0fde
Michal Vyskocil <michal.vyskocil@3key.company>
2026-02-13 16:22:58 +0100

    add createdAt private member - so the Core creation can be tracked

--- src/main/java/com/czertainly/api/model/core/cbom/CbomDto.java
+++ src/main/java/com/czertainly/api/model/core/cbom/CbomDto.java
@@ -17,8 +17,11 @@ public class CbomDto {
 	@Schema(description = "CBOM spec version", requiredMode = Schema.RequiredMode.REQUIRED)
 	private String specVersion;
 
-	@Schema(description = "CBOM timestamp", requiredMode = Schema.RequiredMode.REQUIRED)
+	@Schema(description = "CBOM metadata.timestamp", requiredMode = Schema.RequiredMode.REQUIRED)
 	private OffsetDateTime timestamp;
+
+	@Schema(description = "CBOM timestamp (ala when it was created in Core)", requiredMode = Schema.RequiredMode.REQUIRED)
+	private OffsetDateTime createdAt;
 
 	@Schema(description = "CBOM source (e.g.: CBOM-Lens)", requiredMode = Schema.RequiredMode.REQUIRED)
 	private String source;
