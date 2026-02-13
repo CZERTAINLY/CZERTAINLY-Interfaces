0e11db0bdae2846c9252b14f3c85f67bfa43d3ee
Michal Vyskocil <michal.vyskocil@3key.company>
2026-02-13 18:34:25 +0100

    fix comment

--- src/main/java/com/czertainly/api/model/core/cbom/CbomDto.java
+++ src/main/java/com/czertainly/api/model/core/cbom/CbomDto.java
@@ -17,7 +17,7 @@ public class CbomDto {
 	@Schema(description = "CBOM spec version", requiredMode = Schema.RequiredMode.REQUIRED)
 	private String specVersion;
 
-	@Schema(description = "CBOM metadata.timestamp", requiredMode = Schema.RequiredMode.REQUIRED)
+	@Schema(description = "A timestamp from CBOM metadata", requiredMode = Schema.RequiredMode.REQUIRED)
 	private OffsetDateTime timestamp;
 
 	@Schema(description = "Creation timestamp in Core", requiredMode = Schema.RequiredMode.REQUIRED)
