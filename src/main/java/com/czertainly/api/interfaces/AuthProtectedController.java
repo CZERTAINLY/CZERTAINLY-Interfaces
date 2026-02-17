ae6c7a8877b14bcb37bdb4e88973a1f33bc5d715
lubomirw <76479559+lubomirw@users.noreply.github.com>
2026-02-17 06:26:25 +0100

    Add OpenAPI security schemes and servers to controller parent interfaces

--- src/main/java/com/czertainly/api/interfaces/AuthProtectedController.java
+++ src/main/java/com/czertainly/api/interfaces/AuthProtectedController.java
@@ -3,15 +3,51 @@ package com.czertainly.api.interfaces;
 import com.czertainly.api.config.OpenApiConfig;
 import com.czertainly.api.model.common.AuthenticationServiceExceptionDto;
 import com.czertainly.api.model.common.ErrorMessageDto;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.web.bind.annotation.RestController;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(
+                        url = "https://demo.czertainly.online/api",
+                        description = "CZERTAINLY Demo server"
+                )
+        }
+)
 @RestController
+@SecuritySchemes(value = {
+        @SecurityScheme(
+                name = OpenApiConfig.BEARER_JWT_SECURITY_SCHEME_NAME,
+                type = SecuritySchemeType.HTTP,
+                scheme = "Bearer",
+                bearerFormat = "JWT"
+        ),
+        @SecurityScheme(
+                name = OpenApiConfig.CERTIFICATE_SECURITY_SCHEME_NAME,
+                type = SecuritySchemeType.APIKEY,
+                in = SecuritySchemeIn.HEADER,
+                paramName = "ssl-client-cert",
+                description = "Base64 encoded X.509 certificate passed in header"
+        ),
+        @SecurityScheme(
+                name = OpenApiConfig.SESSION_SECURITY_SCHEME_NAME,
+                type = SecuritySchemeType.APIKEY,
+                in = SecuritySchemeIn.COOKIE,
+                paramName = "czertainly-session", // Name of the cookie containing the session ID
+                description = "Session-based authentication with session ID stored in 'czertainly-session' cookie"
+        ),
+})
 @SecurityRequirements(value = {
         @SecurityRequirement(name = OpenApiConfig.BEARER_JWT_SECURITY_SCHEME_NAME),
         @SecurityRequirement(name = OpenApiConfig.CERTIFICATE_SECURITY_SCHEME_NAME),
