11787fe3edb96889656d7465f4b9870cb3b98d6a
lubomirw <76479559+lubomirw@users.noreply.github.com>
2026-02-17 07:10:18 +0100

    Add OpenAPI security schemes and servers to controller parent interfaces (#596)

--- src/main/java/com/czertainly/api/interfaces/connector/common/v2/NoAuthConnectorController.java
+++ src/main/java/com/czertainly/api/interfaces/connector/common/v2/NoAuthConnectorController.java
@@ -1,14 +1,24 @@
 package com.czertainly.api.interfaces.connector.common.v2;
 
 import com.czertainly.api.model.common.error.ProblemDetailExtended;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RestController;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(
+                        url = "https://demo.czertainly.online",
+                        description = "CZERTAINLY Demo server"
+                )
+        }
+)
 @RestController
 @SecurityRequirements
 @ApiResponses(
