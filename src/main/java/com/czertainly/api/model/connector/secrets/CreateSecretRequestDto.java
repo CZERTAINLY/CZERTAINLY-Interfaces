0df893cf50b327c748c7f01e9a346c9085207a46
Ivo Raisr <ivosh@ivosh.net>
2026-02-25 10:33:28 +0100

    Remove unused imports

--- src/main/java/com/czertainly/api/model/connector/secrets/CreateSecretRequestDto.java
+++ src/main/java/com/czertainly/api/model/connector/secrets/CreateSecretRequestDto.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/com/czertainly/api/model/connector/secrets/UpdateSecretRequestDto.java
+++ src/main/java/com/czertainly/api/model/connector/secrets/UpdateSecretRequestDto.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
