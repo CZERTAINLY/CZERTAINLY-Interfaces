08c5d84fb12bf1fe3389173d0e39b14ff84788de
Ivo Raisr <ivosh@ivosh.net>
2026-03-04 21:45:01 +0100

    Remove unused imports

--- src/main/java/com/czertainly/api/interfaces/core/web/LoginController.java
+++ src/main/java/com/czertainly/api/interfaces/core/web/LoginController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
--- src/main/java/com/czertainly/api/model/core/auth/LoginProviderDto.java
+++ src/main/java/com/czertainly/api/model/core/auth/LoginProviderDto.java
@@ -2,7 +2,6 @@ package com.czertainly.api.model.core.auth;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
