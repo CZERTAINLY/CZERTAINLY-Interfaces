129200f2f5b6c405cd20992d2279f9af3264680e
Michal Vyskocil <michal.vyskocil@3key.company>
2026-03-04 14:32:18 +0100

    use LinkedHashMap

--- src/main/java/com/czertainly/api/model/core/cbom/CbomUploadRequestDto.java
+++ src/main/java/com/czertainly/api/model/core/cbom/CbomUploadRequestDto.java
@@ -1,14 +1,14 @@
 package com.czertainly.api.model.core.cbom;
 
+import java.util.LinkedHashMap;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-
-import java.util.Map;
 
 @Data
 public class CbomUploadRequestDto {
 
 	@Schema(description = "Raw JSON content of CBOM document", requiredMode = Schema.RequiredMode.REQUIRED)
-	private Map<String, Object> content;
+	private LinkedHashMap<String, Object> content;
 
 }
