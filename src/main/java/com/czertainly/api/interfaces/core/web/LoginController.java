47b2127fe249f1009ad7aed533375ab818872058
Ivo Raisr <ivosh@ivosh.net>
2026-03-04 20:56:37 +0100

    Document existing LoginController from Core

---
+++ src/main/java/com/czertainly/api/interfaces/core/web/LoginController.java
@@ -0,0 +1,48 @@
+package com.czertainly.api.interfaces.core.web;
+
+import com.czertainly.api.model.core.auth.LoginProviderDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@Tag(name = "Login Management", description = "Login Management API")
+public interface LoginController {
+    @Operation(summary = "Get available login providers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved available login providers"),
+            @ApiResponse(responseCode = "400", description = "Error during authentication")
+    })
+    @GetMapping(value = "/login", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    List<LoginProviderDto> login(HttpServletRequest request, @RequestParam(value = "error", required = false) String error);
+
+    @Operation(summary = "Login with provider")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login with provider successful"),
+            @ApiResponse(responseCode = "400", description = "Error during authentication")
+    })
+    @GetMapping("/oauth2/authorization/{provider}/prepare")
+    void loginWithProvider(@PathVariable String provider, @RequestParam(value = "redirect", required = false) String redirect, HttpServletResponse response, HttpServletRequest request) throws IOException;
+
+    @Operation(summary = "Get JWK Set of a provider")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "JWK Set retrieved"),
+            @ApiResponse(responseCode = "422", description = "Unable to retrieve JWK Set for the given provider")
+    })
+    @GetMapping(value = "/oauth2/{provider}/jwkSet", produces = {MediaType.APPLICATION_JSON_VALUE})
+    ResponseEntity<String> getJwkSet(@PathVariable String provider);
+}
---
+++ src/main/java/com/czertainly/api/model/core/auth/LoginProviderDto.java
@@ -0,0 +1,26 @@
+package com.czertainly.api.model.core.auth;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+@Data
+@EqualsAndHashCode
+@Schema(description = "Login provider information")
+public class LoginProviderDto {
+    @Schema(description = "Provider name", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String name;
+
+    @Schema(description = "Provider login URL", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String loginUrl;
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("name", name)
+                .append("loginUrl", loginUrl)
+                .toString();
+    }
+}
