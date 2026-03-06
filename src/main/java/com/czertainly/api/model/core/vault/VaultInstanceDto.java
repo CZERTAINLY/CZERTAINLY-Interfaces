6000d519bfdc62955df100af2592227131aee02d
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-03-06 15:51:29 +0100

    Add connector interface to VaultInstanceDto (#619)

--- src/main/java/com/czertainly/api/model/core/vault/VaultInstanceDto.java
+++ src/main/java/com/czertainly/api/model/core/vault/VaultInstanceDto.java
@@ -1,6 +1,7 @@
 package com.czertainly.api.model.core.vault;
 
 import com.czertainly.api.model.common.NameAndUuidDto;
+import com.czertainly.api.model.core.connector.v2.ConnectorInterfaceDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -14,5 +15,8 @@ public class VaultInstanceDto extends NameAndUuidDto {
 
     @Schema(description = "Connector associated with this Vault instance", requiredMode = Schema.RequiredMode.REQUIRED)
     private NameAndUuidDto connector;
+
+    @Schema(description = "Connector Interface associated with this Vault instance", requiredMode = Schema.RequiredMode.REQUIRED)
+    private ConnectorInterfaceDto connectorInterface;
 
 }
