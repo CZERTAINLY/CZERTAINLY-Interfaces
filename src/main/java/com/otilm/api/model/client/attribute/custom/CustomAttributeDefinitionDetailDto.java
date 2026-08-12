585dbb55d12182fceb34ed3dc0bf6b70ee7ba4d1
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-08-12 15:49:12 +0200

    Add property `required` to Custom Attribute listing DTO (#842)
    * Add required to custom attribute listing DTO
    
    * Apply mvn spotless

--- src/main/java/com/otilm/api/model/client/attribute/custom/CustomAttributeDefinitionDetailDto.java
+++ src/main/java/com/otilm/api/model/client/attribute/custom/CustomAttributeDefinitionDetailDto.java
@@ -1,9 +1,7 @@
 package com.otilm.api.model.client.attribute.custom;
 
-import com.otilm.api.model.client.attribute.AttributeDefinitionDto;
 import com.otilm.api.model.common.attribute.common.AttributeContent;
 import com.otilm.api.model.common.attribute.common.AttributeType;
-import com.otilm.api.model.core.auth.Resource;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.Getter;
@@ -13,7 +11,7 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 
 @Getter
 @Setter
-public class CustomAttributeDefinitionDetailDto extends AttributeDefinitionDto {
+public class CustomAttributeDefinitionDetailDto extends CustomAttributeDefinitionDto {
 
     /**
      * Type of the Attribute. For the custom attribute, the type will always be "custom"
@@ -21,12 +19,6 @@ public class CustomAttributeDefinitionDetailDto extends AttributeDefinitionDto {
     @Schema(description = "Type of the Attribute", requiredMode = Schema.RequiredMode.REQUIRED, examples = {
             "custom"}, defaultValue = "custom")
     private AttributeType type;
-
-    /**
-     * Boolean determining if the Attribute is required. If true, the Attribute must be provided.
-     **/
-    @Schema(description = "Boolean determining if the Attribute is required. If true, the Attribute must be provided.", requiredMode = Schema.RequiredMode.REQUIRED)
-    private boolean required;
 
     /**
      * Boolean determining if the Attribute is visible and can be displayed, otherwise it should be hidden to the user.
@@ -65,14 +57,6 @@ public class CustomAttributeDefinitionDetailDto extends AttributeDefinitionDto {
     @Schema(description = "Predefined content for the attribute if needed. The content of the Attribute must satisfy the type")
     private List<AttributeContent> content;
 
-    /**
-     * List of resources
-     *
-     * @return
-     */
-    @Schema(description = "List of resources which are allowed to use the Custom Attribute")
-    private List<Resource> resources;
-
     @Schema(description = "Boolean determining if a list Attribute can have values other than predefined options")
     private boolean extensibleList;
 
@@ -87,8 +71,8 @@ public class CustomAttributeDefinitionDetailDto extends AttributeDefinitionDto {
                 .append("list", list)
                 .append("multiSelect", multiSelect)
                 .append("content", content)
-                .append("resources", resources)
-                .append("required", required)
+                .append("resources", getResources())
+                .append("required", isRequired())
                 .append("extensibleList", extensibleList)
                 .toString();
     }
--- src/main/java/com/otilm/api/model/client/attribute/custom/CustomAttributeDefinitionDto.java
+++ src/main/java/com/otilm/api/model/client/attribute/custom/CustomAttributeDefinitionDto.java
@@ -3,9 +3,12 @@ package com.otilm.api.model.client.attribute.custom;
 import com.otilm.api.model.client.attribute.AttributeDefinitionDto;
 import com.otilm.api.model.core.auth.Resource;
 import io.swagger.v3.oas.annotations.media.Schema;
-
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class CustomAttributeDefinitionDto extends AttributeDefinitionDto {
 
     /**
@@ -14,11 +17,10 @@ public class CustomAttributeDefinitionDto extends AttributeDefinitionDto {
     @Schema(description = "List of resources for custom attribute", requiredMode = Schema.RequiredMode.REQUIRED)
     private List<Resource> resources;
 
-    public List<Resource> getResources() {
-        return resources;
-    }
+    /**
+     * Boolean determining if the Attribute is required. If true, the Attribute must be provided.
+     **/
+    @Schema(description = "Boolean determining if the Attribute is required. If true, the Attribute must be provided.", requiredMode = Schema.RequiredMode.REQUIRED)
+    private boolean required;
 
-    public void setResources(List<Resource> resources) {
-        this.resources = resources;
-    }
 }
